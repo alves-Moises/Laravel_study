22b48d434e53bedb55b3314a13b728d0d62fd46a
alves-Moises <moisesinho_ra@hotmail.com>
2024-02-25 19:03:51 -0300

    add: new files

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// match, any
+// resource
+// Route::get('/sobrenos', [Controller::class, 'teste']);
+
+Route::get('/sobrenos', function(){
+    return 'oi';
+});
+
+Route::any('/salvar', [Controller::class, 'salvar']);
+
+
+
