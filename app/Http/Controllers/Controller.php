eaa3e11a8c5ef27f58cb52f552f210b98e28022c
alves-Moises <moisesinho_ra@hotmail.com>
2024-02-25 21:26:36 -0300

    new controllers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,4 +11,28 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    public function teste(Request $request)
+    {
+        return redirect('sucesso');
+    }
+
+    public function testesucesso(Request $request)
+    {
+        echo "Essa rota foi direcionada!";
+    }
+
+    public function usuario(Request $request)
+    {
+        echo $request->id;
+        // if(isset($request->id)){
+        //     return "Esse é o usuário ".$request->id;
+        // }else{
+        //     return "Trata-se de um novo usuario";
+        // }
+    }
+
+    public function pagina404(Request $request)
+    {
+        return 'Essa página não foi encontrada';
+    }
 }
