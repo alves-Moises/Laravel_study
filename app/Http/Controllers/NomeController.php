a15a29e0672f0c66026050c68e0088b2ab1b13b5
alves-Moises <moisesinho_ra@hotmail.com>
2024-02-29 14:05:42 -0300

    new: NomeController

---
+++ app/Http/Controllers/NomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Nome;
+use Illuminate\Http\Request;
+
+class NomeController extends Controller
+{
+    //pesquisa opcional
+    public function pesquisa(Request $request)
+    {
+        
+        
+        $nomes = Nome::where('nome', 'like', '%'.$request->name.'%')
+            ->when($request->startDate, function($query) use($request){
+                $starDate = new \DateTime($request->startDate);
+                $endDate = new \DateTime($request->endDate);
+                $query('data', '>=', $starDate->format('Y-m-d').' 00:00:00');
+                $query('data', '<=', $endDate->format('Y-m-d').' 23:59:59');;
+
+            })
+            ->get();
+
+        dd($nomes);
+    }
+}
