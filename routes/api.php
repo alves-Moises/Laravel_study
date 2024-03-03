576df44b406b396ac22162ec2bf0169a0e9e382e
alves-Moises <moisesinho_ra@hotmail.com>
2024-03-03 13:44:06 -0300

    update api route test

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/v1/get-user', function() {
+
+    return json_encode(
+        ['nome'=>'thais', 'idade'=>20]
+    );
 });
+
+
+// Route::get('/teste', TesteController::class);
+
+// Route::get('/dados', [TodoController::class, 'dados']);
+
+// Route::get('/relacionamento',[TodoController::class, 'pegarRelacionamento']);
+
+// Route::resource('/fotos', NovoController::class);
+// Route::fallback([Controller::class, 'pagina404']);
+
+
+// Route::get('/pesquisa', [Controller::class, 'pesquisa']);
+// Route::post('/pesquisa', [NomeController::class, 'pesquisa']);
+
+// Route::post('/inserir', [TodoController::class, 'inserir'])->name('inserir');
