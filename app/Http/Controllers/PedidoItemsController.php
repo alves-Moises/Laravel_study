d6beca21c8f0e38a86d81a5fee42cd4ca2ead7e6
alves-Moises <moisesinho_ra@hotmail.com>
2024-03-03 21:54:25 -0300

    new controller: PedidoItemsController

---
+++ app/Http/Controllers/PedidoItemsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PedidoItems;
+use Illuminate\Http\Request;
+
+class PedidoItemsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PedidoItems  $pedidoItems
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PedidoItems $pedidoItems)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PedidoItems  $pedidoItems
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PedidoItems $pedidoItems)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PedidoItems  $pedidoItems
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PedidoItems $pedidoItems)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PedidoItems  $pedidoItems
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PedidoItems $pedidoItems)
+    {
+        //
+    }
+}
