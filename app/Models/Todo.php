a59bdbbe907652b9fd3d02a89c94a71ba4547271
alves-Moises <moisesinho_ra@hotmail.com>
2024-03-03 21:52:38 -0300

    update model: Todo

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -4,18 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
-    use HasFactory;
-
+    use HasFactory, SoftDeletes;
+    // use SoftDeletes;
     
     // protected $tabel = 'todos2';
     // protectecd $primaryKey = 'todosId';
     // public $timestamps = false;
-    // protected  $guarded = [];
-    protected $fillabe = [
-        'nome', 
-        'votos'
-    ];
+    protected  $guarded = [];
+    // protected $fillabe = ['nome', 'votos', "user_id"];
 }
