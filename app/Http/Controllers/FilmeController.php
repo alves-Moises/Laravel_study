f654d4e44dbab17e97b9634de4a40ff13207a47a
alves-Moises <moisesinho_ra@hotmail.com>
2024-03-06 19:51:43 -0300

    new: filmeController

---
+++ app/Http/Controllers/FilmeController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Filme;
+use Illuminate\Http\Request;
+
+class FilmeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('filmes');
+    }
+
+   
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $categorias = $request->categoria;
+        $filmes = Filme::whereIn('categoria', $categorias)->get();
+        
+        $html = 'Os filmes selecionados são: <br>';
+        foreach($filmes as $filme){
+            $html .= $filme->nome.'<br>';
+        }
+        echo $html;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Filme  $filme
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Filme $filme)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Filme  $filme
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Filme $filme)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Filme  $filme
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Filme $filme)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Filme  $filme
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Filme $filme)
+    {
+        //
+    }
+}
