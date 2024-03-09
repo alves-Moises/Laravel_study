36dd3dae3d8c7f85067676883d1d89f7840cf6ed
alves-Moises <moisesinho_ra@hotmail.com>
2024-03-09 14:16:22 -0300

    new factory: TodoFactory

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
+ */
+class TodoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nome'=>fake()->name(),
+            'votos'=>fake()->randomDigit(),
+            'user_id'=>User::factory()
+
+
+        ];
+    }
+}
