e90d1c753d58545ae1115d3a874610c41fb503e0
alves-Moises <moisesinho_ra@hotmail.com>
2024-04-14 19:58:38 -0300

    update form carros

--- resources/views/carros.blade.php
+++ resources/views/carros.blade.php
@@ -1,9 +1,34 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .alert-danger {
+            color: #721c24;
+            background-color: #f8d7da;
+            border-color: #f5c6cb;
+        }
+        .alert {
+            position: relative;
+            padding: 0.75rem 1.25rem;
+            margin-bottom: 1rem;
+            border: 1px solid transparent;
+            border-radius: 0.25rem;
+        }
+    </style>
+</head>
+<body>
+    
+    
+    <form name="form" id="form" enctype="multipart/form-data" method="post" action="/processa">
+        @csrf
+        <input type="file" name="arquivo" id="arquivo" accept="image/jpeg">
+        <input type="submit" value="Enviar">
+    </form>
 
-@section('title', 'Minha Primeira Página')
-
-@section('div-alert')
-    @parent 
-    Sucesso <br>
-    CVc~e efetuou a inserção com sucesso!
-@endsection
+</body>
+</html>
