ba0ee78f3bc9cba5e8853dadaf8366b30c451ef8
alves-Moises <moisesinho_ra@hotmail.com>
2024-04-14 19:54:37 -0300

    new: CursoController

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    public function bemVindo()
+    {
+        return "hELLO WORD";
+    }
+}
