459496182ade7de006d71b59af45ae889189539e
alves-Moises <moisesinho_ra@hotmail.com>
2024-04-14 19:55:58 -0300

    update TodoController

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Todo;
-use Doctrine\DBAL\Schema\View;
+use App\Http\Requests\ThiagoRequest;
+use Illuminate\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -83,13 +84,48 @@ class TodoController extends Controller
         Todo::where('id', '=', $request->id)->delete();
     }
 
-    //retona primeira view
-    public function index()
-    {
+    // //retona primeira view
+    // public function index()
+    // {
 
-        $data = Todo::orderby('nome')->get();
+    //     $data = Todo::orderby('nome')->get();
+    //     return view('carros', compact('data'));
+    // }
+
+    public function index():View
+    {
+        $data = Todo::orderBy('nome')->get();
         return view('carros', compact('data'));
     }
+
+    public function processa(Request $request)
+    {
+        if($request->hasFile('arquivo')){
+            $arquivo = $request->file('arquivo');
+            $extensao = $arquivo->getClientOriginalExtension();
+            if($extensao == 'jpg'){
+                $arquivo->move(public_path().'/images/', rand(0,100).'-'.$arquivo->getClientOriginalName());
+
+                // return 'Hello World';
+
+                // return response('Hello World', 200)->header('Content-Type', 'text/plain');
+
+                // return response('Hello World')->cookie('name', 'value', '300');
+
+                // return redirect('pedidos');
+                // return redirect()->route('pedidos.index');
+                // return back();
+                // return back()->with('status','Dados inseridos com sucesso!');
+                // return view('filmes');
+                // return response()->json([
+                //     'name'=>'Thiago'
+                // ]);
+            }else{
+                echo 'não é uma imagem';
+            }
+        }
+    }
+
 }
 
 
